3ea28fa33afdef8f9d48d27df8a024a11dc17f97
Evans Roy <evansroysir@gmail.com>
2022-04-20 22:53:59 +0300

    initial commit

---
+++ app/src/main/java/com/example/chat/Fragments/ChatsFragment.java
@@ -0,0 +1,108 @@
+package com.example.chat.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chat.Adapter.UserAdapter;
+import com.example.chat.Model.Chat;
+import com.example.chat.Model.Chatlist;
+import com.example.chat.Model.User;
+import com.example.chat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChatsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    private UserAdapter userAdapter;
+    private List<User> mUser;
+
+    FirebaseUser fUser;
+    DatabaseReference reference;
+
+    private List<Chatlist> usersList;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        usersList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chatlist").child(fUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                usersList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chatlist chatlist = snapshot.getValue(Chatlist.class);
+                    usersList.add(chatlist);
+                }
+
+                chatList();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void chatList() {
+        mUser = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUser.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    for (Chatlist chatlist : usersList){
+                        if (user.getId().equals(chatlist.getId())){
+                            mUser.add(user);
+                        }
+                    }
+                }
+                userAdapter = new UserAdapter(getContext(), mUser,true);
+                recyclerView.setAdapter(userAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/chat/Notifications/Client.java
@@ -0,0 +1,16 @@
+package com.example.chat.Notifications;
+
+/**public class Client {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String url){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder(){
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+            }
+            return retrofit
+        }
+    }
+}**/
